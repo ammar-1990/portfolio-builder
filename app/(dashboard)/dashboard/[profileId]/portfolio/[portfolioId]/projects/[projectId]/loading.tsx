c27c307edda9f60138a9117d1014355dfbd84ae8
ammar <ammar.ali.haidar.1990@gmail.com>
2023-10-27 16:33:23 +0400

    work

---
+++ app/(dashboard)/dashboard/[profileId]/portfolio/[portfolioId]/projects/[projectId]/loading.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { Skeleton } from "@/components/ui/skeleton"
+
+
+type Props = {}
+
+const MainLoading = (props: Props) => {
+  return (
+    <div className='w-full h-full flex gap-6 flex-col'>
+<Skeleton className="w-full h-[100px] rounded-xl" />
+<Skeleton className="w-full flex-1 rounded-xl" />
+    </div>
+  )
+}
+
+export default MainLoading
