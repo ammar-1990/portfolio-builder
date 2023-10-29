b6c58448fafc8c1d34540fcfb1b4b4c549df5da2
ammar <ammar.ali.haidar.1990@gmail.com>
2023-10-29 11:55:51 +0400

    work

--- components/themes/(colorful)/(components)/experience.tsx
+++ components/themes/(colorful)/(components)/experience.tsx
@@ -4,14 +4,34 @@ import { Experience } from "@prisma/client";
 import { motion } from "framer-motion";
 import ExperienceComponent from "./experience-component";
 import { cn } from "@/lib/utils";
+import { useRef } from "react";
+import { Button } from "@/components/ui/button";
 
 type Props = { experiences: Experience[]; preview?: boolean };
 
 const ExperienceColorful = ({ experiences, preview }: Props) => {
 
   
+      const containerRef = useRef<HTMLDivElement | null>(null)
+      const handleNextClick = () => {
+
+
+        containerRef.current?.scrollBy({
+          left: 300,
+          behavior: "smooth",
+        });
+    
+      };
+      const handleBackClick = () => {
+    
       
-      
+    
+        containerRef.current?.scrollBy({
+          left:-300,
+          behavior: "smooth",
+        });
+     
+      };
 
   return (
     <div className="sticky top-0 h-screen bg-yellow-200 text-slate-800 text-4xl font-bold z-0 p-3 ">
@@ -23,12 +43,15 @@ const ExperienceColorful = ({ experiences, preview }: Props) => {
       >
         experience
       </motion.h2>
-
+      <div className="max-w-[300px] w-full flex items-center justify-between mx-auto">
+        <Button variant={'ghost'} className="uppercase" onClick={handleBackClick}>back</Button>
+        <Button variant={'ghost'} className="uppercase" onClick={handleNextClick}>next</Button>
+        </div>
       <motion.div
-     
+     ref = {containerRef}
       initial="hidden"
       whileInView="show"
-      className={cn("mt-10 flex max-w-[1200px] mx-auto  py-3 overflow-x-auto snap-x snap-mandatory  sm:px-8 gap-8 max-h-[400px] overflow-y-auto myScroll",preview && 'sm:grid-cols-1 md:grid-cols-1')}>
+      className={cn("mt-10 flex max-w-[1200px] mx-auto  py-3 overflow-x-auto snap-x snap-mandatory  sm:px-8 gap-8 max-h-[400px] overflow-y-auto noScroll",preview && 'sm:grid-cols-1 md:grid-cols-1')}>
         {experiences.map((experience) => (
          <ExperienceComponent key={experience.id} experience={experience} />
         ))}
--- components/themes/(colorful)/(components)/projects.tsx
+++ components/themes/(colorful)/(components)/projects.tsx
@@ -3,9 +3,10 @@
 import { cn } from "@/lib/utils";
 import { Project, Image as PImage } from "@prisma/client";
 import Image from "next/image";
-import React from "react";
+import React, { useRef, useState } from "react";
 import ProjectComponent from "./project-component";
 import { motion } from "framer-motion";
+import { Button } from "@/components/ui/button";
 
 type ProjectWithImages = Project & { images: PImage[] };
 type Props = {
@@ -14,6 +15,30 @@ type Props = {
 };
 
 const ProjectsColorful = ({ projects, preview }: Props) => {
+  const containerRef = useRef<HTMLDivElement | null>(null)
+
+
+
+  const handleNextClick = () => {
+
+
+    containerRef.current?.scrollBy({
+      left: 300,
+      behavior: "smooth",
+    });
+
+  };
+  const handleBackClick = () => {
+
+  
+
+    containerRef.current?.scrollBy({
+      left:-300,
+      behavior: "smooth",
+    });
+ 
+  };
+
   return (
     <div
       id="projects"
@@ -25,15 +50,22 @@ const ProjectsColorful = ({ projects, preview }: Props) => {
          initial={{ opacity: 0, x: -100 }}
          whileInView={{ opacity: 1, x: 0 }}
          transition={{  duration: 1 }}
-      className={cn("text-white text-lg sm:text-3xl md:text-5xl lg:text-7xl p-8 sm:p-20 uppercase", preview && "text-lg sm:text-lg md:text-lg lg:text-lg")}>
+      className={cn("text-white text-lg sm:text-3xl md:text-5xl lg:text-7xl p-8 sm:p-20 pb-4 sm:pb-4 uppercase", preview && "text-lg sm:text-lg md:text-lg lg:text-lg")}>
         projects
       </motion.h2>
-        <div className="mt-10 flex items-center max-w-[1100px] mx-auto overflow-x-auto myScroll gap-4 p-3 snap-mandatory  snap-x overflow-y-hidden">
+      <div className="max-w-[300px] w-full flex items-center justify-between mx-auto">
+        <Button variant={'ghost'} className="uppercase" onClick={handleBackClick}>back</Button>
+        <Button variant={'ghost'} className="uppercase" onClick={handleNextClick}>next</Button>
+        </div>
+        <div ref={containerRef} className="mt-2 flex items-center max-w-[1100px] mx-auto overflow-x-auto noScroll gap-4 p-3 snap-mandatory  snap-x overflow-y-hidden">
           {" "}
-          {projects.map((project) => (
+          {projects.map((project,i) => (
        <ProjectComponent preview={preview} project={project} key={project.id} />
           ))}
         </div>
+      
+
+
     
     </div>
   );
