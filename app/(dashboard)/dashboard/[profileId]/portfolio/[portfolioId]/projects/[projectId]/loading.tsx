056423463bbedd088c27f8bcd0990b6cd578dbfd
ammar <ammar.ali.haidar.1990@gmail.com>
2023-11-04 22:01:14 +0400

    work

--- app/(dashboard)/dashboard/[profileId]/portfolio/[portfolioId]/projects/[projectId]/loading.tsx
+++ app/(dashboard)/dashboard/[profileId]/portfolio/[portfolioId]/projects/[projectId]/loading.tsx
@@ -9,7 +9,7 @@ const MainLoading = (props: Props) => {
   return (
     <div className='w-full h-full flex gap-6 flex-col'>
 <Skeleton className="w-full h-[100px] rounded-xl" />
-<Skeleton className="w-full flex-1 rounded-xl" />
+<Skeleton className="w-full h-[400px] rounded-xl" />
     </div>
   )
 }
